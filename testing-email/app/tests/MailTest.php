8638597b5d60ab55935164e90112881ca620e350
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-05-03 17:39:36 +0200

    Tests working using Mailcatcher

--- testing-email/app/tests/MailTest.php
+++ testing-email/app/tests/MailTest.php
@@ -1,20 +1,50 @@
 <?php
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Message\Response;
+
 class MailTest extends TestCase {
+
+    protected $mailcatcher;
+
+    function __construct()
+    {
+        $this->mailcatcher = new Client(['base_url' => 'http://localhost:1080']);
+    }
+
+    public function getAllEmails()
+    {
+        $emails = $this->mailcatcher->get('/messages')->json();
+
+        if (empty($emails)) {
+            $this->fail('No messages returned.');
+        }
+
+        return $emails;
+    }
+
+    public function getLastEmail()
+    {
+        $emailId = $this->getAllEmails()[0]['id'];
+
+        return $this->mailcatcher->get("/messages/{$emailId}.html");
+    }
+
+    public function assertEmailBodyContains($body, Response $email)
+    {
+        $this->assertContains($body, (string) $email->getBody());
+    }
 
     /**
      * @test
      */
     function it_sends_an_email()
     {
-        $mock = Mockery::mock('Swift_Mailer');
-        $this->app['mailer']->setSwiftMailer($mock);
-
-        $mock->shouldReceive('send')->once()->andReturnUsing(function ($message) {
-            $this->assertArrayHasKey('joe@example.com', $message->getTo());
-        });
-
         $this->call('GET', 'emailtest');
+
+        $email = $this->getLastEmail();
+
+        $this->assertEmailBodyContains('Welcome', $email);
     }
 
 }
---
+++ testing-email/app/views/emails/welcome.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome!</h1>
+
+    <p>Thanks for joining my site.</p>
+</body>
+</html>
