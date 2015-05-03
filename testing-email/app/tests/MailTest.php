fc648780fc8e601f20b2742f056bc88fd154985b
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-05-03 17:49:59 +0200

    Added external class for helper test methods

--- testing-email/app/tests/MailTest.php
+++ testing-email/app/tests/MailTest.php
@@ -1,39 +1,6 @@
 <?php
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Message\Response;
-
-class MailTest extends TestCase {
-
-    protected $mailcatcher;
-
-    function __construct()
-    {
-        $this->mailcatcher = new Client(['base_url' => 'http://localhost:1080']);
-    }
-
-    public function getAllEmails()
-    {
-        $emails = $this->mailcatcher->get('/messages')->json();
-
-        if (empty($emails)) {
-            $this->fail('No messages returned.');
-        }
-
-        return $emails;
-    }
-
-    public function getLastEmail()
-    {
-        $emailId = $this->getAllEmails()[0]['id'];
-
-        return $this->mailcatcher->get("/messages/{$emailId}.html");
-    }
-
-    public function assertEmailBodyContains($body, Response $email)
-    {
-        $this->assertContains($body, (string) $email->getBody());
-    }
+class MailTest extends MailTestCase {
 
     /**
      * @test
@@ -45,6 +12,7 @@ class MailTest extends TestCase {
         $email = $this->getLastEmail();
 
         $this->assertEmailBodyContains('Welcome', $email);
+        $this->assertEmailWasSentTo('joe@example.com', $email);
     }
 
 }
